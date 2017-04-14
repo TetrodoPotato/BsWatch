5ac3c4d2b8741deca83831dea488330b2bd6a188
Kartoffeleintopf <guardblade@gmx.de>
2017-04-15 01:56:45 +0200

    Add files via upload

--- bsWatchCon1.user.js
+++ bsWatchCon1.user.js
@@ -5,11 +5,13 @@
 // @include     https://bs.to/
 // @include     https://bs.to/home
 // @include     https://bs.to/home/
-// @version    	1
+// @version    	1.1
 // @description	Redirect for bs home.
 // @author     	Kartoffeleintopf
 // @run-at 		document-start
 // @downloadURL https://raw.githubusercontent.com/Kartoffeleintopf/BsWatch/master/bsWatchCon1.user.js
 // ==/UserScript==
 
+
+console.log('yay');
 window.location = 'https://bs.to/serie-alphabet';
