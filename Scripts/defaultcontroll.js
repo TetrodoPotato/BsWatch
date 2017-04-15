4dfc4f434c1511297b5d477c6bf4a0f9c09921fd
Kartoffeleintopf <guardblade@gmx.de>
2017-04-15 04:17:23 +0200

    Add files via upload

--- Scripts/defaultcontroll.js
+++ Scripts/defaultcontroll.js
@@ -1,2 +1,3 @@
-var cssLink = 'https://rawgit.com/Kartoffeleintopf/BsWatch/master/StyleSheeds/bsStyle.css';
+//var cssLink = 'https://rawgit.com/Kartoffeleintopf/BsWatch/master/StyleSheeds/bsStyle.css';
+var cssLink = 'https://dl.dropbox.com/s/rf3rzlzsa3lg3to/bsStyle.css';
 var cssVidLink = 'https://cdn.rawgit.com/Kartoffeleintopf/BsWatch/master/StyleSheeds/playerStyle.css';
