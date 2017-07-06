ee3f1c58ca73f0f1dc19fa622c1c991c87922a53
Kartoffeleintopf <guardblade@gmx.de>
2017-07-07 00:21:14 +0200

    Easy Install

---
+++ Instructions/install.bat
@@ -0,0 +1,12 @@
+CD C:\Program Files\Mozilla Firefox\
+start firefox.exe https://raw.githubusercontent.com/Kartoffeleintopf/BsWatch/master/BsWatch1.user.js
+start firefox.exe https://raw.githubusercontent.com/Kartoffeleintopf/BsWatch/master/BsWatch2.user.js
+start firefox.exe https://raw.githubusercontent.com/Kartoffeleintopf/BsWatch/master/BsWatch3.user.js
+start firefox.exe https://raw.githubusercontent.com/Kartoffeleintopf/BsWatch/master/BsWatch4.user.js
+start firefox.exe https://raw.githubusercontent.com/Kartoffeleintopf/BsWatch/master/BsWatch5.user.js
+start firefox.exe https://raw.githubusercontent.com/Kartoffeleintopf/BsWatch/master/BsWatch6.user.js
+start firefox.exe https://raw.githubusercontent.com/Kartoffeleintopf/BsWatch/master/BsWatch7.user.js
+start firefox.exe https://raw.githubusercontent.com/Kartoffeleintopf/BsWatch/master/BsWatch8.user.js
+start firefox.exe https://raw.githubusercontent.com/Kartoffeleintopf/BsWatch/master/BsWatch9.user.js
+start firefox.exe https://raw.githubusercontent.com/Kartoffeleintopf/BsWatch/master/BsWatch10.user.js
+exit;
