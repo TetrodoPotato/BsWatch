f909488538f04357a91c24024bfcbfc175039688
Kartoffeleintopf <guardblade@gmx.de>
2017-08-02 05:53:00 +0200

    Add files via upload

--- Scripts/logStorage.js
+++ Scripts/logStorage.js
@@ -2,10 +2,10 @@ function addLog(seriesName, seasonName, episodNameGer, episodNameOri, genresName
 	var addString = seriesName + "|" + seasonName + "|" + episodNameGer + "|" + episodNameOri + "|" + genresName + "|" + hosterName + "|" + dateName;
 
 	var logData = getRawLog();
+	
 	logData.push(addString);
-
 	logData = clearLog(logData);
-
+	
 	localStorage.setItem('log', JSON.stringify(logData));
 }
 
@@ -58,10 +58,10 @@ function clearLog(logData) {
 	//20170806
 	var bufArr = [];
 	for (i = 0; i < logData.length; i++) {
-		var logDate = logData.split('|')[6];
-		var buf = logDate.split(' ').split(',');
+		var logDate = logData[i].split('|')[6];
+		var buf = logDate.split(' ')[0].split(',');
 		buf = parseInt(buf[2] + buf[1] + buf[0]);
-
+		
 		if ((dateNum - buf) < 100) {
 			bufArr[bufArr.length] = logData[i];
 		}
