ac5b2614a9b5caec7f4722935f94ceccb9e3a89e
Kartoffeleintopf <guardblade@gmx.de>
2017-09-23 04:53:57 +0200

    FIX
    init.js needs cookieControll.js

--- BsWatch7.user.js
+++ BsWatch7.user.js
@@ -8,6 +8,7 @@
 // @author     	Kartoffeleintopf
 // @run-at 		document-start
 // @require 	https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js
+// @require		https://raw.githubusercontent.com/Kartoffeleintopf/BsWatch/master/Scripts/cookiecontroll.js
 // @require		https://raw.githubusercontent.com/Kartoffeleintopf/BsWatch/master/Scripts/init.js
 // @downloadURL https://raw.githubusercontent.com/Kartoffeleintopf/BsWatch/master/BsWatch7.user.js
 // ==/UserScript==
